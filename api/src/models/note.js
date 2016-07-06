1034fff0e7f7dc1c71ebda33a80094ac1e2dfe55
iftruereturn <qwertyzxc11@gmail.com>
2016-07-06 23:04:58 +0300

    Add note model

---
+++ api/src/models/note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  value: { type: String, default: '' },
+  updated: { type: Date, default: Date.now },
+  tags: { type: Array },
+  owner: { type: String, default: 'Anonymous' },
+  private: { type: Boolean, default: false }
+});
+
+NoteSchema.pre('save', (next) => {
+  this.updated = new Date();
+  next();
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,31 @@
-var koa = require('koa');
-var route = require('koa-route');
-var app = module.exports = koa();
+const koa = require('koa');
+const route = require('koa-route');
+const app = module.exports = koa();
+
+const parse = require('co-body');
+
+// db connect
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/notebook_online');
+
+const Note = require('./models/note.js');
 
 app.use(route.get('/', function*() {
-  this.body = 'Hello World!';
+  const allNotes = yield Note.find({}).exec();
+
+  this.set({ 'Content-Type': 'application/json' });
+  this.body = JSON.stringify(allNotes);
 }));
+
+app.use(route.post('/', function*() {
+  const note = yield parse(this);
+  const newNote = new Note(note);
+
+  yield newNote.save();
+
+  this.redirect('/');
+}));
+
 
 app.listen(3000);
 console.log('The app is listening on port 3000');
