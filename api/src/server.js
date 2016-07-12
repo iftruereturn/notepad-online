22970c26560d3c04864e561e8498f9250d7fb7f5
iftruereturn <qwertyzxc11@gmail.com>
2016-07-12 23:16:26 +0300

    Remove webpack middleware from server

--- api/src/server.js
+++ api/src/server.js
@@ -1,14 +1,17 @@
 const koa = require('koa');
 const app = module.exports = koa();
 
+/*
 const send = require('koa-send');
 const serve = require('koa-static');
+*/
 
 // db connect
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/notebook_online');
 
 // hot-reload webpack
+/*
 const webpack = require('webpack');
 const webpackConfig = require('./../../webpack.config');
 const compiler = webpack(webpackConfig);
@@ -17,14 +20,17 @@ app.use(function*(next) {
   yield hotMiddleware.bind(null, this.request, this.response);
   yield next;
 });
+*/
 
 // routes
 require('./routes/router')(app);
 
+/*
 app.use(function* index() {
   console.log('Responded with html');
   yield send(this, __dirname + './../../index.html');
 })
+*/
 
 app.listen(3000);
 console.log('The app is listening on port 3000');
