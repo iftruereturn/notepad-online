bef7a5707ee7e307a5ab367a7cd16ee6d6c8ef9f
iftruereturn <qwertyzxc11@gmail.com>
2016-08-20 00:48:29 +0300

    Remove footer

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Auth from '../containers/Auth';
-import Footer from './Footer';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
         <Auth></Auth>
-        <div className="content">
-          {this.props.children}
-        </div>
-        <Footer></Footer>
+        {this.props.children}
       </div>
     );
   }
